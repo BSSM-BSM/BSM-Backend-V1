a86b094ca2f1050e7e122b2d8927124f9e9adc9f
SW2125\dltjd <leehj050211@gmail.com>
2021-10-16 20:50:22 +0900

    express 프레임워크 적용

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req ,res) => res.send('Hello World!'))
+app.set('view engine', 'ejs')
+app.set('views', './views/pages');
+app.use(express.static('public'));
+
+app.get('/', (req ,res) => res.render('index', {}))
+app.get('/meal', (req ,res) => res.render('meal', {}))
+app.get('/timetable', (req ,res) => res.render('timetable', {}))
 
 app.listen(80)
