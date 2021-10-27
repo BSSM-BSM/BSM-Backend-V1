5a2cfb78bf8a2814a2a254ed08a0e6c11a4a7ebb
SW2125\dltjd <leehj050211@gmail.com>
2021-10-27 17:32:34 +0900

    댓글 쓰기

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -22,8 +22,24 @@ const view = async (req, res) =>{
     }
     res.send(JSON.stringify(result))
 }
-const write = (req, res) =>{
-    res.send()
+const write = async (req, res) =>{
+    let model = require('../../models/comment')
+    let boardType, commentBoardType;
+    switch(req.params.boardType){
+        case 'board':
+            boardType='board'
+            commentBoardType='board_comment'
+            break;
+        case 'anonymous':
+            boardType='anonymous'
+            commentBoardType='anonymous_comment'
+            break
+    }
+    dbResult = await model.write(boardType, commentBoardType, req.params.postNo, req.session.memberCode, req.session.memberNickname, req.body.comment)
+    result={
+        status:dbResult
+    }
+    res.send(JSON.stringify(result))
 }
 const del = (req, res) =>{
     res.send()
--- models/comment.js
+++ models/comment.js
@@ -21,7 +21,27 @@ const view = (commentBoardType, postNo) => {
         })
     })
 }
-const write = (boardType) => {
+const write = (boardType, commentBoardType, postNo, memberCode, memberNickname, comment) => {
+    let commentIndexQuery="SELECT `order` from `"+commentBoardType+"` where `depth`=0 and `post_no`="+postNo+" order by `comment_index` desc limit 1;"
+    return new Promise(resolve => {
+        conn.query(commentIndexQuery, (error, order) => {
+            if(error) resolve(error)
+            if(order[0].order==null){
+                order = 1;
+            }else{
+                order = order[0].order+1;
+            }
+            commentWriteQuery="INSERT INTO `"+commentBoardType+"` (`post_no`, `depth`, `order`, `parent_no`, `member_code`, `member_nickname`, `comment`, `comment_date`) VALUES ("+postNo+", 0, "+order+", NULL, "+memberCode+", '"+memberNickname+"', '"+comment+"', now());"
+            conn.query(commentWriteQuery, (error, results) => {
+                if(error) resolve(error)
+                commentUpdateQuery = "UPDATE `"+boardType+"` set `post_comments`=`post_comments`+1 where `post_no`="+postNo
+                conn.query(commentUpdateQuery, (error, result) => {
+                    if(error) resolve(error)
+                    resolve(1)
+                })
+            })
+        })
+    })
 }
 const del = (boardType) => {
 }
